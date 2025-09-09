70b2db319e636e39d0fdd7451426d5519d85172f
Akila fernando <akila.fernando14@gmail.com>
2025-09-09 09:18:18 +0530

    test

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <p className="bg-amber-700">Hello World</p>
+      <p className="bg-amber-700">Hello World, EDULearngit</p>
     </div>
   )
 }
